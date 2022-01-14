93fd9f71de80eede873d662a1cdaa683c8cbcd4a
Muhammet Ali <94957965+MuhammetAli54@users.noreply.github.com>
2022-01-14 15:08:19 +0300

    Dasboard eklendi

---
+++ OduncKitapASPNETMVCWebSolution_UI/Controllers/DashBoardController.cs
@@ -0,0 +1,22 @@
+﻿using OduncKitapASPNETMVCWebSolution_BLL.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OduncKitapASPNETMVCWebSolution_UI.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        //Global alan
+        KitapManager myKitapManager = new KitapManager();
+        // GET: DashBoard
+        public ActionResult Index()
+        {
+            ViewBag.BugunEklenenKitapSayisi = myKitapManager.TumAktifKitaplariGetir().Where(x => x.KayitTarihi > DateTime.Now.AddDays(-1) && x.KayitTarihi < DateTime.Now.AddDays(1)).Count();
+
+            return View();
+        }
+    }
+}
---
+++ OduncKitapASPNETMVCWebSolution_UI/Controllers/PartialsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OduncKitapASPNETMVCWebSolution_BLL.Managers;
+using OduncKitapASPNETMVCWebSolution_UI.Controllers;
+
+namespace OduncKitapASPNETMVCWebSolution_UI.Controllers
+{
+    public class PartialsController : Controller
+    {
+        //Global alan
+        KitapManager myKitapManager = new KitapManager();
+        //Tüm partial'ları tek bir yerden yönetmek amacıyla partialsController oluşturduk.
+        public PartialViewResult MenuPartialResult()
+        {
+            int toplamKitapSayisi = myKitapManager.TumAktifKitaplariGetir().Count();
+            TempData["ToplamKitapSayisi"] = toplamKitapSayisi;
+
+            return PartialView("_PartialMenu");
+        }
+    }
+}
